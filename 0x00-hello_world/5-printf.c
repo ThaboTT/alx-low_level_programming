3739e7d6ddd5037fdd14909afa04e383a4e434f3
Thabott <thaboncube8@live.com>
2022-09-13 11:39:10 -0700

    File to print c code using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * input2 - variable to store  string
+ * retun: Always 0 (Success)
+ */
+int main(void)
+{
+	char input2[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", input2);
+
+	return (0);
+}
