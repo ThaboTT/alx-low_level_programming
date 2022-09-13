9b6708ff1829a99bda7cfe972574582278365777
Thabott <thaboncube8@live.com>
2022-09-13 10:53:49 -0700

    C file to print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * input1 - variable to store a string
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char input1[60];
+
+	strcpy(input1, "\"Programming is like building multilingual puzzle");
+
+	puts(input1);
+
+	return (0);
+}
