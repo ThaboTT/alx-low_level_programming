788173246c47ae2b678a0ebab928cb5ad1433a7f
Thabott <thaboncube8@live.com>
2022-09-20 04:17:07 -0700

    Code to print a combination of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Aways 0 (Success)
+*/
+
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		if (i != '9')
+		{
+			putchar(i);
+			putchar(',');
+			putchar(' ');
+		}
+		else
+			putchar(i);
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
