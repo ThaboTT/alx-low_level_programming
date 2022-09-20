852397d8000ed3b53f948dded000efe9b1ce41d8
Thabott <thaboncube8@live.com>
2022-09-20 01:28:05 -0700

    C code to print alphabet wihtout q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'q' || i == 'e')
+		{
+			continue;			
+		}
+
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
