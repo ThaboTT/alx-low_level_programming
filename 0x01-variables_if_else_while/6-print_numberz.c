829333162496f56963f3e4326f591b501c73857f
Thabott <thaboncube8@live.com>
2022-09-20 03:05:05 -0700

    Code to print 0-9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main -Entry Point
+*
+* Reurn: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	/* Use ASCII to translate 0-9 values */
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
