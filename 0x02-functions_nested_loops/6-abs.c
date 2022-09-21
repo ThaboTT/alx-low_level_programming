640d1233605cf2d1cef08be2ef67d1a00f33a04b
Thabott <thaboncube8@live.com>
2022-09-21 14:06:43 -0700

    Function to get absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _abs - converts to an absolute value
+* @c: - to be made absolute
+*
+* Return: absolute int
+*/
+
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (-c);
+}
