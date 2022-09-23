45cfc06836df9c9a3256922a9aa93405aeacb75a
Thabott <thaboncube8@live.com>
2022-09-23 05:40:52 -0700

    Function to print the diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_diagonal - print \ diagonally
+* @n: determines how many backlashes to print
+*
+* Return: void
+*/
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= i; j++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
