04786cc6579d7252a12e6df17cce45c540c4e4ba
Thabott <thaboncube8@live.com>
2022-09-25 13:03:44 -0700

    Task 9 code

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int m3;
+	int m5;
+
+	for (i = 1; i <= 100; i++)
+	{
+		m3 = i % 3;
+		m5 = i % 5;
+
+		if (m3 == 0)
+			printf("%s ",  "Fizz");
+		else if (m5 == 0)
+			printf("%s ", "Buzz");
+		else if (m3 == 0 && m5 == 0)
+			printf("%s ", "FizzBuzz");
+		else
+			printf("%d ", i);
+	}
+	putchar('\n');
+	
+	return (0);
+}
