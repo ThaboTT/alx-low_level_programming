8c11d2e59839629bd86b6863ab7602fb724571b3
Thabott <thaboncube8@live.com>
2022-09-25 08:29:00 -0700

    Fixed code for task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,10 +17,11 @@ void print_diagonal(int n)
 	{
 		for (i = 1; i <= n; i++)
 		{
-			for (j = 1; j <= i; j++)
-				_putchar(92);
-			/*_putchar(92);*/
+			for (j = 1; j < i; j++)
+				_putchar(' ');
+			_putchar(92);
 			_putchar('\n');
 		}
+		
 	}
 }
